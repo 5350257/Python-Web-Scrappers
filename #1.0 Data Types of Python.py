fc5425caf3e73b8cbe4e3505841a96b836405887
5350257 <collar5192@gmail.com>
2021-09-27 22:46:19 +0900

    python #1.0 커밋

---
+++ #1.0 Data Types of Python.py
@@ -0,0 +1,17 @@
+a_int = 2
+b_long = 3123123423423312
+
+
+a_stirng = "Like this"
+
+d_boolean = True
+e_boolean = False
+
+f_float = 3.123
+g_double = 21421.21
+
+print(type(a_int))
+print(type(b_long))
+
+h_none = None
+# none is none only had python
